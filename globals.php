59a18921aff16bfa45d683dbe8186dd07baa4c58
Mauri Sahlberg <mos@iki.fi>
2015-09-09 20:10:48 +0300

    Korjauksia.

--- globals.php
+++ globals.php
@@ -9,26 +9,36 @@
  * */
 
 ini_set("variables_order","EGPCS");
-$dbuser=getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME') ? getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME') : 'sls';
-$dbpassword=getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD") ? getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD") : 'foobar';
-$dburl=getenv("OPENSHIFT_POSTGRESQL_DB_URL");
-$dbport=getenv("OPENSHIFT_POSTGRESQL_DB_PORT") ? getenv("OPENSHIFT_POSTGRESQL_DB_PORT") : "5432";
-$dbhost=getenv("OPENSHIFT_POSTGRESQL_DB_HOST") ? getenv("OPENSHIFT_POSTGRESQL_DB_HOST") : "10.1.0.110";
-$dbhost="192.168.98.58";
-$dbname="slskirjasto";
+ini_set('session.referer_check', "");
 
-$dsn="pgsql:host=$dbhost;port=$dbport;dbname=$dbname";
-$baseurl=getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME') ? "http://slskirjasto-claymountain.rhcloud.com" : "http://localhost/~mos/slskirjasto";
+$dbuser=getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME');
+if($dbuser===false) {
+    define("ENV","Test");    
+    $dbuser='sls';
+    $dburl="";
+    $dbport="5432";
+    $dbhost="localhost";
+    $dbname="slskirjasto";
+    $baseurl="http://localhost/slskirjasto";
+}
+else {
+    define("ENV", "Production");
+    $dbpassword=getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD");
+    $dburl=getenv("OPENSHIFT_POSTGRESQL_DB_URL");
+    $dbport=getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+    $dbhost=getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+    $baseurl="http://slskirjasto-claymountain.rhcloud.com";
+}
 $basepath=__DIR__;
 
-/** @var SESSION_TIMEOUT int Istunnon kesto sekunneissa */
+require_once("$basepath/private.php");
+
+$dsn="pgsql:host=$dbhost;port=$dbport;dbname=$dbname";
+
 define("SESSION_TIMEOUT", 40*60);
-/** @var SESSION_NAME string Istunnon nimi */
 define("SESSION_NAME", "slskirjasto");
-/** @var SESSION_COOKIEPATH string Evästeen polku */
 define("SESSION_COOKIEPATH", "/");
-/** @var REDIRECT_URI string Googlen autentikaation urli */
 define("REDIRECT_URI", "$baseurl/google_login.php");
-ini_set('session.referer_check', "");
+
 define("URL_MUNGLER", true);
 ?>
