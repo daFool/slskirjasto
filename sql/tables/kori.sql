d8e31d02bbe5579b2863a36cdc72717a7d4ee8de
Mauri Sahlberg <mos@iki.fi>
2017-09-13 23:20:25 +0300

    Järjestelty sql-hakemistoa uusiksi ja heivattu tavaraa ullakolle.

--- sql/tables/kori.sql
+++ sql/tables/kori.sql
@@ -3,19 +3,24 @@ drop table if exists Kori;
 create table Kori (
         ID serial,
         omistaja varchar(255),
-        paivitetty timestamp with time zone default now(),
-        lisatty timestamp with time zone default now(),
         nimi varchar(255) unique not null,
+        
+        like Pohjat INCLUDING ALL,
         
         constraint id_pkey primary key (ID),
         constraint omistaja_fkey foreign key (omistaja) references kayttaja(tunniste) on update cascade on delete cascade
         );
-        
+ comment on table Kori is 'Pelikori';
+ comment on column Kori.ID is 'Keinotekoinen avain';
+ comment on column Kori.omistaja is 'Korin omistajan tunniste';
+ comment on column Kori.nimi is 'Korin nimi';
  
 
  create table KorinPelit (
         ID bigint,
         peli varchar(255),
+        
+        like Pohjat INCLUDING ALL,
         constraint peli_fkey foreign key (peli) references kokoelmapeli(tunniste) on update cascade on delete cascade,        
         constraint kori_fkey foreign key (ID) references Kori(ID) on update cascade on delete cascade
         );       
--- sql/vKayttajaRoolit.sql
+++
@@ -1,3 +0,0 @@
-drop view if exists vKayttajaRoolit;
-create view vKayttajaRoolit as select u.Tunniste, u.Nimi, u.Tila, kr.Kohde, kr.Roolinimi from Kayttaja as u join KayttajaRooli as kr on u.tunniste=kr.Kayttaja;
-grant all on vKayttajaRoolit to sls;
