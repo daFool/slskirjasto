34225a688ee6e4291001c3c02eb61ee27d7326b2
Mauri Sahlberg <mos@iki.fi>
2018-08-26 17:26:12 +0300

    Ant hoitaa dokumentaation ja ajaa sonarquben. Ecosysteemiin tarkistus siitä, että symlinkki ja SONARLOGIN on asetettu.

--- bin/ecosystem.sh
+++ bin/ecosystem.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
-which composer >/dev/null 2>/dev/null
-if [ $? -ne 0 ]; then
-    echo "You should install composer! Composer is required to pull in vendor code!"
-    exit -1;
+
+function haveIt {
+    which $1 >& /dev/null
+    if [ $? -ne 0 ]; then
+        echo $2
+        exit -1;
+    fi
+}
+
+mepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+mepath=${mepath%/*}
+
+if [ ! -L "$mepath/web/SLS/controller" ]; then
+	echo "Hmm. No symlink $mepath/web/SLS/controller to $mepath/web/controller?"
+	echo "I'm a coward. Please do symlink for me. The F3 will not find classes without it!"
+	exit -1;
 fi
-which phpdox >/dev/null 2>/dev/null
-if [ $? -ne 0 ]; then
-    echo "Phpdox is used for documentation generation. Please install it."
+
+haveIt composer         "You should install composer! Composer is required to pull in vendor code!"
+haveIt phpdox           "Phpdox is used for documentation generation. Please install it."
+haveIt phpunit          "Phpunit is used for testing - nah who am I kidding, will be used. Please install it."
+haveIt phpdoc           "phpDocumentor is not installed. Please install it."
+haveIt pdepend          "pdepend is not installed. Please install it."
+haveIt phpcpd           "phpcpd is not installed."
+haveIt phploc           "phploc is not installed."
+haveIt phpmd            "phpmd is not installed."
+haveIt ant              "Come on! Ant is not installed. Try harder!"
+haveIt sonar-scanner    "No sonar-scanner, are you kidding me?"
+
+if [ -z "$SONARLOGIN" ]; then
+	echo "Sonarqube can't login without env $SONARLOGIN set!"
+	exit -1
 fi
-which phpunit >/dev/null 2>/dev/null
-if [ $? -ne 0 ]; then
-    echo "Phpunit is used for testing - nah who am I kidding, will be used. Please install it."
-fi
-which phpdoc >&/dev/null
-if [ $? -ne 0 ]; then
-	echo "phpDocumentor is not installed. Please install it."
-fi
+haveIt beard            "Beard. Just say no thanks."
